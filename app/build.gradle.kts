f93cbea011380030db885935db7647d37c51b81c
hassnainy1 <hassnainmalik306@gmail.com>
2025-07-28 16:56:30 +0500

    UPDATE COMMIT

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
 }
 
 dependencies {
+    implementation("com.google.android.material:material:1.11.0")
     implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-inappmessaging-display:20.3.3")
